e398eab5b10d79b5681252a6881f4a321357964b
rafaadail <rafaadailph@gmail.com>
2019-07-01 19:36:17 -0300

    Configrando navegação.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Routes from './routes';
 
 import "./styles.css";
 
@@ -7,8 +8,8 @@ import Main from './pages/main';
 
 const App = () => (
     <div className="App">
-        <Header/>
-        <Main/>
+        <Header />
+        <Routes />
     </div>
 );
 
