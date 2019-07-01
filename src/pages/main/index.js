3faac17326841ba244276b8e8157e37f9e3620bd
rafaadail <rafaadailph@gmail.com>
2019-07-01 15:58:31 -0300

    Estilização Listando Produtos.

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import api from "../../services/api";
 
+import './styles.css';
+
 export default class Main extends Component {
     state = {
         products: []
@@ -17,10 +19,17 @@ export default class Main extends Component {
 
     //         return (<h1>Contagem de produtos: {this.state.products.length}</h1>
     render() {
+        const { product } = this.state;
+
         return (
             <div className="product-list">
                 {this.state.products.map(product => (
-                    <h2 key={product._id}>{product.title}</h2>
+                    <article key={product._id}>
+                    <strong>{product.title}</strong>
+                    <p>{product.description}</p>
+
+                    <a href="">Acessar</a>
+                    </article>
                 ))}
             </div>
         );
